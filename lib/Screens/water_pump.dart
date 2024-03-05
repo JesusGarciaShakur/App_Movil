c24908c0ee33a52621d9ebfc6c28e0187a55c447
alvaro aguirre <alguirre2@gmail.com>
2024-03-05 00:04:45 -0600

    07.1 upgrate water_pump_control

--- lib/Screens/water_pump.dart
+++ lib/Screens/water_pump.dart
@@ -1,6 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
 class WaterPump extends StatefulWidget {
@@ -11,6 +10,12 @@ class WaterPump extends StatefulWidget {
 }
 
 class _WaterPumpState extends State<WaterPump> {
+  // variable para controlar el controlador dela barra
+double linearValue= 0.5;
+double linearValue2= 0.5;
+
+
+
 // crear variable para  traer el tiempo
 TimeOfDay _timeOfDay =const  TimeOfDay(hour: 8, minute: 30);
 
@@ -44,45 +49,116 @@ TimeOfDay _timeOfDay =const  TimeOfDay(hour: 8, minute: 30);
                                     )
                             ),
 
-                      Text(_timeOfDay.format(context).toString(),
-                        style:const TextStyle(fontSize: 57)),
+                    Text(_timeOfDay.format(context).toString(),
+                      style:const TextStyle(fontSize: 57)),
 
-                        MaterialButton(
-                          onPressed:_showTimePicker,
-                          child:const Text("un boton")
+                    MaterialButton(
+                      onPressed:_showTimePicker,
+                      child:const Text("un boton")
+                      ),
+
+                    Text("Encender con el contenido en: ${(linearValue*100).toStringAsFixed(0)}%"),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("0%"),
+                          GestureDetector(
+                              onTapUp: (details) {
+                              double tapPosition = details.localPosition.dx;
+                                linearValue = tapPosition / (MediaQuery.of(context).size.width-120);
+                              setState(() {
+                                    linearValue = linearValue.clamp(0.0, 1.0);
+                                          }
+                                      );
+                              },
+                               onPanUpdate: (details) {
+                                    linearValue += details.delta.dx / (MediaQuery.of(context).size.width - 100);
+                                  setState(() {
+                                    if (linearValue < 0) {
+                                      linearValue = 0.0;
+                                    } else if (linearValue > 1) {
+                                      linearValue = 1;
+                                    }
+                                  });
+                                },
+
+                                child:LinearPercentIndicator(
+                                width: MediaQuery.of(context).size.width-100,
+                                animation: true,
+                                animationDuration: 0,
+                                lineHeight: 30,
+                                percent: linearValue,
+                                barRadius: const Radius.circular(100),
+                                progressColor: const Color.fromARGB(255, 169, 160, 190),
+                              ),
+
                           ),
+                          const Text("100%")
+                        ],
+                      ),
 
-                        // CircularPercentIndicator(
-                        //   animation: true,
-                        //   animationDuration: 1000,
-                        //   radius: 100,
-                        //   lineWidth: 20,
-                        //   percent: .5,
-                        //   progressColor: const Color.fromARGB(255, 131, 232, 231),
-                        //   backgroundColor: const Color.fromARGB(249, 157, 181, 178),
-                        //   circularStrokeCap: CircularStrokeCap.round,
-                        //   center: const Text("50%",style: TextStyle(fontSize: 60)),
-                        //  ),
+                    ),
 
-                        const Text("apagar con el porcentaje en "),
-                        LinearPercentIndicator(
-                          animation: true,
-                          animationDuration: 1000,
-                          lineHeight: 30,
-                          percent: 0.5,
-                          progressColor: const Color.fromARGB(255, 169, 160, 190),
-                          
-                        ),
-                        
+
+
+                          // la segunda barra de control 
+                     Text("apagar cuando este lleno al: ${(linearValue2*100).toStringAsFixed(0)}%"),
+                      Padding(
+                      padding: const EdgeInsets.all(20.0),
+
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("0%"),
+                          GestureDetector(
+                              onTapUp: (details) {
+                              double tapPosition = details.localPosition.dx;
+                                linearValue2 = tapPosition / (MediaQuery.of(context).size.width-120);
+                              setState(() {
+                                    linearValue2 = linearValue2.clamp(0.0, 1.0);
+                                          }
+                                      );
+                              },
+                               onPanUpdate: (details) {
+                                    linearValue2 += details.delta.dx / (MediaQuery.of(context).size.width - 100);
+                                  setState(() {
+                                    if (linearValue2 < 0) {
+                                      linearValue = 0.0;
+                                    } else if (linearValue2 > 1) {
+                                      linearValue2 = 1;
+                                    }
+                                  });
+                                },
+
+                                child:LinearPercentIndicator(
+                                width: MediaQuery.of(context).size.width-100,
+                                animation: true,
+                                animationDuration: 0,
+                                lineHeight: 30,
+                                percent: linearValue2,
+                                barRadius: const Radius.circular(100),
+                                progressColor: const Color.fromARGB(255, 169, 160, 190),
+                              ),
+
+                          ),
+                          const Text("100%")
+                        ],
+                      ),
+                   
+                    ),
+
+
+
+                    
                         const MaterialButton(
                           onPressed:null,
                           child:Text("Guardar")
-                          )
-                ],
+                        )
+                  ],
                 ),
-
-
-
               )
 
     );
